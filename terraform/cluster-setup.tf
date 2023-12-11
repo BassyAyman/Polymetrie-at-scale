e73b4d5639b4ad6df33092f19e9ec939df14cf82
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2023-12-11 15:59:32 +0100

    First terraform setup script

--- terraform/cluster-setup.tf
+++ terraform/cluster-setup.tf
@@ -9,3 +9,53 @@ terraform {
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "terraform_test"
+  }
+}
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "my-polymetrie"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    selector {
+      match_labels = {
+        app = "MyPolymetrieApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyPolymetrieApp"
+        }
+      }
+      spec {
+        container {
+          image = "igormel/polymetrie:1.1"
+          name  = "polymetrie-container"
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "test" {
+  metadata {
+    name      = "polymetrie-service"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.test.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "LoadBalancer"
+    port {
+      port        = 80
+      target_port = 8080
+    }
+  }
+}
