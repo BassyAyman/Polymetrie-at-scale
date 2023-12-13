9c4fc68b4eb60de92cb967c8f43b8192185baa55
TobiasBonifay <tobias.bonifay@etu.unice.fr>
2023-12-13 10:20:44 +0100

    hotfix

--- terraform/cluster-setup.tf
+++ terraform/cluster-setup.tf
@@ -33,7 +33,7 @@ resource "kubernetes_deployment" "test" {
       }
       spec {
         container {
-1.          image = "igormel/polymetrie:1.2"
+          image = "igormel/polymetrie:1.2"
           name  = "polymetrie-container"
           port {
             container_port = 8080
