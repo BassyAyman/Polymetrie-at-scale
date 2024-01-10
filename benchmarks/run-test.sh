f355889a2942a71abcfd4d8bbde5ce7493d65af6
IgorMelnykSI <ihor.melnyk@etu.unice.fr>
2024-01-10 20:51:38 +0100

    Better version of run-test script

--- benchmarks/run-test.sh
+++ benchmarks/run-test.sh
@@ -3,11 +3,14 @@
 # Variables
 configmap_name="my-test"
 yaml_file="./k6-resource.yaml"
-wait_time="11s"
+wait_time="60s"
 script_name="$1"
 
+# Création du fichier intermédiaire
+cp "$script_name" test-to-run.js
+
 # Créer le ConfigMap à partir du fichier donné en argument
-kubectl create configmap "$configmap_name" --from-file="$script_name"
+kubectl create configmap "$configmap_name" --from-file=test-to-run.js
 
 # Appliquer le déploiement depuis le fichier k6-resource.yaml
 kubectl apply -f "$yaml_file"
@@ -22,5 +25,8 @@ kubectl delete configmap "$configmap_name"
 
 # Supprimer le déploiement
 kubectl delete -f "$yaml_file"
+
+# Supprimer le script intermédiaire
+rm -f test-to-run.js
 
 echo "Script '$script_name' execution done."
